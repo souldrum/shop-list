1f6aeb0a0078e5e7219d34d17646951efa20c4e0
souldrum <soul_20585@mail.ru>
2025-01-31 09:38:08 +0500

    Сделан первый вариант с dnd

--- src/components/List/AppList.vue
+++ src/components/List/AppList.vue
@@ -1,21 +1,40 @@
 <script setup lang="ts">
 import type { Product } from "@/types/product.types";
+import draggable from "vuedraggable";
 import AppListItem from "../ListItem/AppListItem.vue";
+import { ref } from "vue";
 
 type Props = {
   products: Product[];
 };
 
+type DraggableOptions = {
+  animation?: number;
+  group?: string;
+  disabled?: boolean;
+  ghostClass?: string;
+};
+
+const options = ref<DraggableOptions>({
+  animation: 200,
+  group: "description",
+  disabled: false,
+  ghostClass: "ghost",
+});
+
 const { products } = defineProps<Props>();
 </script>
 
 <template>
-  <ul class="flex flex-col gap-2">
-    <AppListItem
-      v-for="(product, index) of products"
-      :product
-      :index
-      :key="product.id"
-    />
-  </ul>
+  <draggable
+    tag="ul"
+    :list="products"
+    class="flex flex-col gap-2"
+    item-key="id"
+    v-bind="options"
+  >
+    <template #item="{ element: product, index }">
+      <AppListItem :product :index :key="product.id" />
+    </template>
+  </draggable>
 </template>
