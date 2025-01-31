70fae9180348284ba1975c967325582bb99bd811
souldrum <soul_20585@mail.ru>
2025-01-31 10:46:18 +0500

    fix попытка 2 нераспространения dnd на элементы, меняющие статус продукта

--- src/components/ListItem/AppListItem.vue
+++ src/components/ListItem/AppListItem.vue
@@ -103,20 +103,27 @@ const onDelete = (id: string) => {
     </div>
 
     <span v-else class="mr-auto">{{ index + 1 }}. {{ product.title }}</span>
-    <div v-if="!isEdit" class="flex gap-3" @drag.stop>
+    <div v-if="!isEdit" class="flex gap-3">
       <PencilIcon
         class="size-6 cursor-pointer"
         @click="onEdit(product.title)"
+        @drag.stop
       />
       <CheckIcon
         class="size-6 text-green-500 cursor-pointer"
         @click="onDone(product.id)"
+        @drag.stop
       />
       <MinusCircleIcon
         class="size-6 text-error cursor-pointer"
         @click="onCancel(product.id)"
+        @drag.stop
       />
-      <TrashIcon class="size-5 cursor-pointer" @click="onDelete(product.id)" />
+      <TrashIcon
+        class="size-5 cursor-pointer"
+        @click="onDelete(product.id)"
+        @drag.stop
+      />
     </div>
   </li>
 </template>
