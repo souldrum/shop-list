400003f2f73c394e21640ed30980c2ff6f2b2b9e
souldrum <soul_20585@mail.ru>
2025-11-07 00:35:53 +0500

    Небольшой рефакторинг кнопки

--- src/ui/Button/AppButton.vue
+++ src/ui/Button/AppButton.vue
@@ -8,16 +8,14 @@ export type BtnStyleType =
 
 type Props = {
   styleType: BtnStyleType;
-  disabled?: boolean;
-  onClick?: () => void;
 };
 
-const { styleType, disabled } = defineProps<Props>();
+const { styleType } = defineProps<Props>();
 </script>
 
 <template>
   <button
-    class="flex gap-2 justify-center items-center text-label-lg py-2.5 px-6 rounded-full outline-hidden cursor-pointer"
+    class="flex gap-2 justify-center items-center text-label-lg py-2.5 px-6 rounded-full outline-hidden cursor-pointer disabled:text-on-background-op-38 disabled:bg-on-background-op-12 disabled:shadow-none active:disabled:shadow-none active:disabled:bg-on-background-op-12 disabled:cursor-not-allowed"
     :class="{
       'text-on-primary bg-primary shadow-elevation-2 active:shadow-elevation-1':
         styleType === 'filled',
@@ -28,11 +26,7 @@ const { styleType, disabled } = defineProps<Props>();
       'border border-outline bg-inherit text-primary active:backdrop-brightness':
         styleType === 'outlined',
       'bg-inherit text-primary': styleType === 'text',
-      'disabled:text-on-background-op-38 disabled:bg-on-background-op-12 disabled:shadow-none active:disabled:shadow-none active:disabled:bg-on-background-op-12 cursor-not-allowed':
-        disabled,
     }"
-    @click="onClick"
-    :disabled
   >
     <slot>Нажми</slot>
   </button>
